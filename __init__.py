00b4a52510ab36780187224842dc26dc20fe988b
Ashlin Darius Govindasamy <adgrules@hotmail.com>
2021-09-08 16:19:37 +0200

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,44 @@
+# ADGSTUDIOS 2021
+import pyodbc
+class sqlserver():
+    def __init__(self,connectionstring):
+	    self.connectionstring = connectionstring
+            
+    def ExecuteQuery(self, Query):
+        try:
+            conn = pyodbc.connect(self.connectionstring)
+            cursor = conn.cursor()
+            cursor.execute(Query)
+            cursor.commit()
+            print("Query Executed")
+        except Exception as e:
+            print(e)
+    def fields(self, cur):
+        results = {}
+        column = 0
+        for d in cur.description:
+            results[d[0]] = column
+            column = column + 1
+        return results
+    def GetRecordsOfColumn(self, SelectQuery, ColumnName):
+        try:
+            conn = pyodbc.connect(self.connectionstring)
+            cursor = conn.cursor()
+            cursor.execute(SelectQuery)
+            field_map = self.fields(cursor)
+            values = []
+            for row in cursor:
+                values.append(row[field_map[ColumnName]])
+            return values
+        except Exception as e:
+            print(e)
+    def GetRecordsAsDict(self, SelectQuery):
+        try:
+            conn = pyodbc.connect(self.connectionstring)
+            cursor = conn.cursor()
+            cursor.execute(SelectQuery)
+            return {'results':
+            [dict(zip([column[0] for column in cursor.description], row))
+             for row in cursor.fetchall()]}
+        except Exception as e:
+            print(e)
